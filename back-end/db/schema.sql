df053e0ddb68cb657164a12831d7860f980f30ab
Grace Bong <gracebong.park@gmail.com>
2022-01-11 00:23:13 -0500

    cleaned up code and added basic error/success messages

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -5,38 +5,41 @@ CREATE DATABASE mico_db;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    password VARCHAR(50),
     phone_number VARCHAR(12),
     address VARCHAR(100),
-    email VARCHAR(100) UNIQUE NOT NULL,
-    admin BOOLEAN DEFAULT false,
-    bank_id BIGINT REFERENCES bank_accounts(id),
-    property_id BIGINT REFERENCES properties(id),
-    investment_id BIGINT REFERENCES investments(id)
+    email VARCHAR(100) UNIQUE,
+    admin BOOLEAN DEFAULT false
+    -- bank_id BIGINT REFERENCES bank_accounts(id),
+    -- property_id BIGINT REFERENCES properties(id),
+    -- investment_id BIGINT REFERENCES investments(id)
 );
 
 DROP TABLE IF EXISTS bank_accounts;
 CREATE TABLE bank_accounts (
     id BIGSERIAL PRIMARY KEY,
-    bank VARCHAR(50) NOT NULL,
-    type TEXT NOT NULL,
-    amount BIGINT NOT NULL
+    bank VARCHAR(50),
+    type TEXT,
+    amount BIGINT,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS properties;
 CREATE TABLE properties (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    type TEXT NOT NULL,
-    worth BIGINT NOT NULL
+    name VARCHAR(50),
+    type TEXT,
+    worth BIGINT,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS investments;
 CREATE TABLE investments (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    type TEXT NOT NULL,
-    amount BIGINT NOT NULL
+    name VARCHAR(50),
+    type TEXT,
+    amount BIGINT,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
--- back-end/queries/usersQuery.js
+++ back-end/queries/usersQuery.js
@@ -3,56 +3,119 @@ const db = require('../db/dbConfig');
 const getAllUsers = async () => {
     try {
         const allUsers = await db.any('SELECT * FROM users');
-        return allUsers;
+        return {
+            success: true,
+            payload: allUsers
+        };
+
     } catch (error) {
-        return error;
+        return {
+            success: false,
+            payload: error
+        };
     };
 };
 
 const getOneUser = async (id) => {
     try {
-        const oneUser = await db.any('SELECT * FROM users WHERE id=$1', id);
-        return oneUser;
+        const oneUser = await db.one('SELECT * FROM users WHERE user_id=$1', id);
+        return {
+            success: true,
+            payload: oneUser
+        };
+
     } catch (error) {
-        return error;
+        return {
+            success: false,
+            payload: error
+        };
     };
 };
 
 const newUser = async (user) => {
     try {
         const newUser = await db.one(
-            'INSERT INTO users (first_name, last_name, password, phone_number, address, email) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
+            `
+            INSERT INTO users 
+            (first_name, last_name, password, phone_number, address, email) 
+            VALUES
+            ($1, $2, $3, $4, $5, $6) 
+            RETURNING *
+            `,
             [
-                user.first_name, user.last_name, user.password, user.phone_number, user.address, user.email
+                user.first_name, 
+                user.last_name, 
+                user.password, 
+                user.phone_number, 
+                user.address, 
+                user.email
             ]
         );
+        return {
+            success: true,
+            payload: newUser
+        };
+
     } catch (error) {
-        return error;
+        return {
+            success: false,
+            payload: error
+        };
     };
 };
 
 const deleteUser = async (id) => {
     try {
         const deletedUser = await db.one(
-            'DELETE FROM users WHERE id=$1 RETURNING *', id
+            'DELETE FROM users WHERE user_id=$1 RETURNING *', id
         );
-        return deleteUser;
+        return {
+            success: true,
+            payload: deletedUser
+        };
+
     } catch (error) {
-        return error;
+        return {
+            success: false,
+            payload: error
+        };
     };
 };
 
-const updateUser = async (user) => {
+const updateUser = async (id, user) => {
     try {
         const updatedUser = await db.one(
-            'UPDATE users SET first_name=$1, last_name=$2, password=$3, phone_number=$4, address=$5, email=$6',
+            `UPDATE users 
+            SET 
+                first_name=$1, 
+                last_name=$2, 
+                password=$3, 
+                phone_number=$4, 
+                address=$5, 
+                email=$6 
+            WHERE user_id=$7 
+            RETURNING *
+            `,
             [
-                user.first_name, user.last_name, user.password, user.phone_number, user.address, user.email
+                user.first_name, 
+                user.last_name, 
+                user.password, 
+                user.phone_number, 
+                user.address, 
+                user.email, 
+                id
             ]
         );
-        return updatedUser;
+        return {
+            success: true,
+            payload: updatedUser
+        };
+
     } catch (error) {
-        return error;
+        return {
+            success: false,
+            payload: error
+        };
     };
 };
 
