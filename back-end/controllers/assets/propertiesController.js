eea9c73f635a4f95d48c408770676bf57a90f488
Grace Bong <gracebong.park@gmail.com>
2022-02-16 14:37:55 -0500

    get all properties query and controller added

--- back-end/controllers/assets/propertiesController.js
+++ back-end/controllers/assets/propertiesController.js
@@ -1,4 +1,35 @@
 const express = require('express');
 const properties = express.Router({ mergeParams: true });
+const {
+    getAllProperties
+} = require('../../queries/assets/propertiesQuery');
+
+properties.get('/', async (req, res) => {
+    const { user_id } = req.params;
+
+    try {
+        const { success, payload } = await getAllProperties(user_id);
+
+        if (success) {
+            res.status(200).json({
+                success,
+                payload
+            });
+
+        } else {
+            console.error(payload);
+            return {
+                success,
+                payload: `Failed to find properties for user_id: ${user_id}`
+            };
+        };
+
+    } catch (error) {
+        res.status(404).json({
+            error: 'Resources not found',
+            message: error
+        });
+    };
+});
 
 module.exports = properties;
--- back-end/queries/assets/propertiesQuery.js
+++ back-end/queries/assets/propertiesQuery.js
@@ -1,9 +1,27 @@
 const db = require('../../db/dbConfig');
 
+const getAllProperties = async (user_id) => {
+    try {
+        const allProperties = await db.any(
+            `SELECT * FROM properties WHERE user_id=user_id`
+            ,user_id
+        );
+
+        return {
+            success: true,
+            payload: allProperties
+        };
+
+    } catch (error) {
+        return {
+            success: false,
+            payload: error
+        };
+    };
+};
+
+
 module.exports = {
     getAllProperties,
-    getOneProperty,
-    newProperty,
-    deleteProperty,
-    updateProperty
+    
 };
